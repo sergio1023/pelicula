7abee9f63413c4090caaf14b581c817a287474f1
sergio1023 <sergyfutbol37@gmail.com>
2022-03-30 12:57:13 +0200

    readme

--- bdpelicula.sql
+++ bdpelicula.sql
@@ -1,9 +1,20 @@
+drop database if exists cartelera;
+
 create database cartelera;
 
 CREATE TABLE peliculas
-(id_pelicula int,
+(id  MEDIUMINT NOT NULL AUTO_INCREMENT,
 titulo char(50),
 duracion int,
 descripcion char(50),
 caratula char(50),
-trailer char(255));
+trailer char(255)
+PRIMARY KEY (id));
+
+CREATE TABLE usuarios(
+id MEDIUMINT NOT NULL AUTO_INCREMENT,
+login varchar(100),
+pass_hash varchar(255),
+PRIMARY KEY (id)
+);
+
