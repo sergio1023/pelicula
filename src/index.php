4820201d63a32e51d777f71fa41bdad6d7245f61
sergio1023 <sergyfutbol37@gmail.com>
2022-04-13 15:21:37 +0200

    proximas peliculoas añadidas

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
+<?php
+include('acceso_datos.php');
+
+$datos = $conexion_bd->query('SELECT * FROM peliculas');
+
+?>
 <!doctype html>
 <html lang="en" class="h-100">
   <head>
@@ -41,9 +47,29 @@
 <!-- Begin page content -->
 <main class="flex-shrink-0">
   <div class="container">
-    <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-    <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-    <p>Back to <a href="../examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
+  <h1>Próximas Películas</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Título</th>
+                    <th>Duración</th>
+                    <th>Carátula</th>
+                    <th>Tráiler</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($datos as $fila) { ?>
+                <tr>
+                    <td><?=$fila['id']?></td>
+                    <td><?=$fila['titulo']?></td>
+                    <td><?=$fila['duracion']?></td>
+                    <td><?=$fila['caratula']?></td>
+                    <td><?=$fila['trailer']?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
   </div>
 </main>
 
---
+++ src/listado.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Cartelera de cine</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sticky-footer-navbar/">
+
+  
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sticky-footer-navbar.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+
+    <?php include('include_menu.php')?>
+
+
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    Aquí pondremos el listado
+  </div>
+</main>
+
+  <?php include('include_mostrar_cookies.php')?> 
+
+    <script src="/js/bootstrap.min.js"></script>
+      </body>
+</html>
